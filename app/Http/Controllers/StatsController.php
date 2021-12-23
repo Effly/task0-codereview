7c479dadb081fa70cfedecd0d1e082db815362f7
Effly <effly88@gmail.com>
2021-12-24 00:22:32 +0300

    Сделал статистику

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ShortLink;
+use App\Models\Viewers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -11,32 +12,35 @@ class StatsController extends Controller
     public function getStatsByCode($code)
     {
         $link = ShortLink::where('code', $code)->firstOrFail();
-//        dd($link->viewers()->get()->unique('user_agent','ip')->toArray());
-
-        $results = $link->viewers()->orderBy('created_at', 'desc')->get(['user_agent','ip','created_at'])
-            ->unique(function ($item){
-                return $item['user_agent'].$item['ip'];
-            })
+        return $link->viewers()->orderBy('created_at', 'desc')->get(['user_agent', 'ip', 'created_at'])
             ->groupBy(function ($items) {
-                return Carbon::parse($items->created_at)->format('d.m.Y'); // А это то-же поле по нему мы и будем группировать
-            })
-//            ->count();
-            ->toArray();
-        dd($results);
-        $allResults = [];
-        foreach ($results as $date => $result) {
-            dump(($result));
-            $allResults[$date] = [
-                'total_views'=>count($result),
-//                "unique_views"=> ,
+                return Carbon::parse($items->created_at)->format('d.m.Y');
+            })->map(function ($item) {
+                return [
+                    'total_views' => $item->count(),
+                    "unique_views" => $item->unique(function ($item) {
+                        return $item['user_agent'] . $item['ip'];
+                    })->count()
+                ];
 
-            ];
-        }
-        dd($allResults);
+            })
+            ->toArray();
     }
 
     public function getStats()
     {
 
+        $allLinks = ShortLink::all();
+        return [
+            'unique_views' => array_sum($allLinks->map(function ($item) {
+                return $item->viewers()->get()->unique(function ($item) {
+                    return $item['user_agent'] . $item['ip'];
+                })->count();
+            })->toArray()),
+            'total_views' => Viewers::all()->count()
+//        "total_views" => array_sum($allLinks->map(function ($item) { // Вот тут я прям хз как лучше Вроде б и запрос в бд один но действий с коллекцией больше
+//                return $item->viewers()->get()->count();
+//            })->toArray())
+        ];
     }
 }
