5be7e951d0d31b967a19596048ef208c3a3942e8
Effly <effly88@gmail.com>
2021-12-29 08:56:55 +0300

    Минор изменения

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\ShortLinkController;
 */
 
 Route::get('/', function () {
+//    dd(123);
+    return phpinfo();
     return view('welcome');
 });
 
@@ -22,7 +24,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('{code}',[ShortLinkController::class, 'followShortLink'])->name('followShortLink');
+Route::get('/{code}',[ShortLinkController::class, 'followShortLink'])->name('followShortLink');
 
 //Route::get('/stats{id}')
 
